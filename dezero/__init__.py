c449304a385a6c678843fd5ba27e812734db31fe
lots-o <kgh0916@gmail.com>
2023-09-11 23:56:38 +0900

    feat: step32

--- dezero/__init__.py
+++ dezero/__init__.py
@@ -13,9 +13,10 @@ if is_simple_core:
     from dezero.core_simple import as_array
     from dezero.core_simple import as_variable
     from dezero.core_simple import setup_variable
+    from dezero.core_simple import Config
 
 else:
-    # TODO: step33 부터 dezero/core.py 정의
+    # step33 부터 dezero/core.py 정의
     from dezero.core import Variable
     from dezero.core import Parameter
     from dezero.core import Function
@@ -26,21 +27,6 @@ else:
     from dezero.core import as_variable
     from dezero.core import setup_variable
     from dezero.core import Config
-    from dezero.layers import Layer
-    from dezero.models import Model
-    from dezero.datasets import Dataset
-    from dezero.dataloaders import DataLoader
-    from dezero.dataloaders import SeqDataLoader
-
-    import dezero.datasets
-    import dezero.dataloaders
-    import dezero.optimizers
-    import dezero.functions
-    import dezero.functions_conv
-    import dezero.layers
-    import dezero.utils
-    import dezero.cuda
-    import dezero.transforms
 
 setup_variable()
 __version__ = "0.0.13"
