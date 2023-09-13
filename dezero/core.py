1d107c7e30018e02069a3a4c39a47a12994a8e19
lots-o <kgh0916@gmail.com>
2023-09-14 00:12:36 +0900

    fix: incorrect tab indent

--- dezero/core.py
+++ dezero/core.py
@@ -88,17 +88,17 @@ class Variable:
 
             with using_config("enable_backprop", create_graph):
                 gxs = f.backward(*gys)
-            if not isinstance(gxs, tuple):
-                gxs = (gxs,)
+                if not isinstance(gxs, tuple):
+                    gxs = (gxs,)
 
-            for x, gx in zip(f.inputs, gxs):
-                if x.grad is None:
-                    x.grad = gx
-                else:
-                    x.grad = x.grad + gx
+                for x, gx in zip(f.inputs, gxs):
+                    if x.grad is None:
+                        x.grad = gx
+                    else:
+                        x.grad = x.grad + gx
 
-                if x.creator is not None:
-                    add_func(x.creator)
+                    if x.creator is not None:
+                        add_func(x.creator)
 
             if not retain_grad:
                 for y in f.outputs:
